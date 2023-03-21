3a5252c1aa1e1b4383e3f26dd7b07391172f7bfb
Izanarraez <104890144+Izanarraez@users.noreply.github.com>
2023-03-21 21:57:35 +0100

    a

---
+++ Vista/error_404.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <?php require_once("Plantillas/plantillas_bootstrap.html") ?>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <img src="Imgs/logo_google.jpg" class="d-flex w-25">
+                <h2><b>404.</b></h2><p> That's an error.</p>
+                <h5>The requested URL</h5>
+            </div>
+            <div class="col-12">
+                <img src="Imgs" class="d-flex w-25">
+            </div>
+            <div class="col-12">
+
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
