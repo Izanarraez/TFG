be87b81d9cc82b178685ac72ceac951ca9fc51d4
Izanarraez <104890144+Izanarraez@users.noreply.github.com>
2023-04-02 10:55:31 +0200

    f

--- miApp/src/app/app.module.ts
+++ miApp/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponentesComponent } from './componentes/componentes.component';
+import { AgregarEmpleadoComponent } from './componentes/agregar-empleado/agregar-empleado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentesComponent,
+    AgregarEmpleadoComponent
   ],
   imports: [
     BrowserModule
