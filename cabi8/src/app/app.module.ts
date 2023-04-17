d03adaeedaf714d958998c685991732daba7cf5f
Izanarraez <104890144+Izanarraez@users.noreply.github.com>
2023-04-17 15:45:47 +0200

    p

--- cabi8/src/app/app.module.ts
+++ cabi8/src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: '', pathMatch:'full', redirectTo: 'home'},
+  {path: 'home', loadChildren: ()=> import('./modules').then(m => m.HomeModule)},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
+  exports:[],
   providers: [],
   bootstrap: [AppComponent]
 })
